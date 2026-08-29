44c1c13e257e91d7a693d9e778ae5a249126c61e
lodostarim <firatfikri@hotmail.com>
2026-08-29 08:47:52 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Çek/Senet — çevrimdışı çalışma.
    Uygulamayı güncellediğinde SURUM numarasını artır; telefonlar yeni sürümü çeker. */
-const SURUM = "ceksenet-v2";
+const SURUM = "ceksenet-v3";
 const DOSYALAR = ["./", "./index.html", "./manifest.json",
                   "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 
